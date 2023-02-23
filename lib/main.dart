abdb54010f03b8da99eaecb1162cf2201b4916ae
Cypherfelix <felixmarvinonline@gmail.com>
2023-02-23 14:29:26 +0300

    making sizing compatible

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chatty/common/routes/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
@@ -12,22 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: const Size(360, 780),
+      builder: (context, child) => GetMaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
       ),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
     );
   }
 }
--- lib/pages/frame/welcome/view.dart
+++ lib/pages/frame/welcome/view.dart
@@ -1,8 +1,8 @@
 import 'package:chatty/common/style/color.dart';
+import 'package:chatty/common/values/colors.dart';
 import 'package:chatty/pages/frame/welcome/controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class WelcomePage extends GetView<WelcomeController> {
@@ -10,13 +10,15 @@ class WelcomePage extends GetView<WelcomeController> {
 
   Widget _buildPageHeadTitle(String title) {
     return Container(
+      margin: const EdgeInsets.only(top: 20),
       child: Text(
         title,
-        style: const TextStyle(
-          color: AppColor.primaryText,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryElementText,
           fontFamily: "Montserrat",
           fontWeight: FontWeight.bold,
-          fontSize: 45,
+          fontSize: 45.sp,
         ),
       ),
     );
@@ -25,14 +27,12 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primaryElement,
       body: Container(
+        width: 360.w,
+        height: 780.h,
         padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
-        child: Column(
-          children: [
-            _buildPageHeadTitle(controller.title),
-            const Placeholder(),
-          ],
-        ),
+        child: _buildPageHeadTitle(controller.title),
       ),
     );
   }
